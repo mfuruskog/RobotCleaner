c881c48b3ee28f717c0e7142f9330f14fd51f59c
mafu <mafu@netlight.com>
2020-06-24 09:48:26 +0200

    Removed usings

--- RobotCleaner/Line.cs
+++ RobotCleaner/Line.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace RobotCleaner
 {
